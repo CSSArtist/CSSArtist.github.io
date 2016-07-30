999abe017594971e9aba547bdef7322a6e2a1f79
CSSArtist <bearzweb@gmail.com>
2016-07-30 16:06:13 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<!--
+	Website: Bearz Web & GraFX Dezign www.bearzdesignconcepts.net
+    File Name: index.html
+    Author: Barry Smith
+-->
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="keywords" content="Barry Smith, Barry Leroy Smith, Bearz Design Concepts, Bearz Web and Graphics Design, Bearz Design, Graphic Design, Web Design, The Coding Zone, HTML Elements, HTML5 Elements, HTML Tags, HTML Tag Use, Typography, HTML Syntax, CSS Default Display Settings, Coding, Validation, HTML, HTHL5, CSS3, Coding Zone">
+	<meta name="description" content="Simple and affordable alternative to Web Design with the small business in mind">
+	<title>
+    	Barry Leroy Smith - BearZ DeZign Concepts
+    </title>
+			<!-- supporting CSS and JavaScript and favicon files -->
+    <link href="cssPortfolio/leadPage.css" rel="stylesheet" type="text/css">
+	<link href="css3/cssToolTip.css" rel="stylesheet" type="text/css">
+	<link href="css3/myAnimations.css" rel="stylesheet" type="text/css">
+	<link href="cssPortfolio/responsiveCSS.css" rel="stylesheet" type="text/css">
+	<link href="css3/specialEffects.css" rel="stylesheet" type="text/css">
+	<link href="cssPortfolio/specialCSS.css" rel="stylesheet" type="text/css">
+	<link href="TheCSSZone/embeddedFonts/embeddedFonts.css" rel="stylesheet" type="text/css">
+	<link href="TheCSSZone/embeddedFonts/alluraWebReady/allura.css" rel="stylesheet" type="text/css">
+	<link href="TheCSSZone/embeddedFonts/fontSavatage/savatage.css" rel="stylesheet" type="text/css">
+    <link href="icon/iconBDC/apple-icon-57x57.png" rel="apple-touch-icon" sizes="57x57">
+    <link href="icon/iconBDC/apple-icon-60x60.png" rel="apple-touch-icon" sizes="60x60">
+    <link href="icon/iconBDC/apple-icon-72x72.png" rel="apple-touch-icon" sizes="72x72">
+    <link href="icon/iconBDC/apple-icon-76x76.png" rel="apple-touch-icon" sizes="76x76">
+    <link href="icon/iconBDC/apple-icon-114x114.png" rel="apple-touch-icon" sizes="114x114">
+    <link href="icon/iconBDC/apple-icon-120x120.png" rel="apple-touch-icon" sizes="120x120">
+    <link href="icon/iconBDC/apple-icon-144x144.png" rel="apple-touch-icon" sizes="144x144">
+    <link href="icon/iconBDC/apple-icon-152x152.png" rel="apple-touch-icon" sizes="152x152">
+    <link href="icon/iconBDC/apple-icon-180x180.png" rel="apple-touch-icon" sizes="180x180">
+    <link href="icon/iconBDC/android-icon-192x192.png" rel="icon" type="image/png" sizes="192x192">
+    <link href="icon/iconBDC/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32">
+    <link href="icon/iconBDC/favicon-96x96.png" rel="icon" type="image/png" sizes="96x96">
+    <link href="icon/iconBDC/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16">
+    <link href="icon/iconBDC/manifest.json" rel="manifest">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <script src="scripts/siteFunctions.js" type="text/javascript"></script>
+    <script src="scripts/jquery_v2.0.0.js" type="text/javascript"></script>
+    <!-- used for my floating back button in lower right corner -->
+	<script>
+		$(document).ready(function () {  $('.backToTopScroll').hide();//Check to see if the window is top if not then display button
+		$(window).scroll(function () {  if ($(this).scrollTop() > 300) {    $('.backToTopScroll').fadeIn();  } else {    $('.backToTopScroll').fadeOut();  }}); //Click event to scroll to top
+		$('.backToTopScroll').click(function () {  $('html, body').animate({ scrollTop: 0 }, 900);  return false;  });});
+    </script>
+</head>
+<body id="bearzHome"><a class="backToTopScroll" onClick="closeHomeSection()">Page Top</a><!-- -->
+	<h1 class="displayNone">BearZ DeZign Concepts</h1>
+	<section id="homeWrapper" class="homeWrapper flipInHome"><!--  -->
+        <header id="BannerLogo" class="headerBackground">
+        <h2 class="displayNone">BearZ DeZign Concepts</h2>
+            <img src="images/BDC.png" alt="Bearz Dezign Concepts">
+        </header>
+        <section>
+        	<header id="introHeader" class="intro">
+            	<h2 class="welcomeMsg1">Welcome To the website of</h2>
+            	<p class="welcomeMsg2">Barry L&egrave;roy Smith</p><p class="miniWelcomeMsg">and</p>
+            	<p class="welcomeMsg2">My Portfolio</p>
+                <noscript>
+                     <p>If you are seeing this message<b>&#44;</b> JavaScript has been disabled in you<b class="apos"></b>re browser<b>&#46;</b><br>
+                     Here are the <a class="textLink" href="http://www.enable-javascript.com/" target="_blank"><span>www.enable-javascript.com</span>instructions how to enable JavaScript</a> in your web browser<b>&#46;</b></p>
+                </noscript>
+            </header>
+<!-- About Me -->
+            <article id="aboutMe" class="articleSubSeparator homeFadeIn"><!-- displayNone -->
+                <h2 class="home2">Who I am</h2>
+                <img class="myImage" src="images/myPic.jpg" alt="Barry Smith">
+                <div class="articleSeparator">
+                    <p class="homeText">
+                        <span class="firstLetter">M</span>y name is <span class="italicsBold">Barry Smith&#46;</span>&ensp;I am an Artist<b>&#33;</b>&ensp;I <em>Love</em> drawing<b>&#44;</b> painting<span class="ellipsis"></span> <span class="italicsCaps">creating something beautiful from the mundane&#46;&ensp;</span>I can find myself at my drawing table<b>&#44;</b> in front of a canvas<b>&#44;</b> or in my sculpture studio for hours at a time<b>&#44;</b> completely in my own world<b>&#46;</b>&ensp;My approach to computer design is the same<b>&#46;</b>&ensp;I find myself sitting in front of a computer screen for hours<b>&#44;</b> completely in my own world<b>&#46;</b>&ensp;<u>Focused&#46;</u> and <u>In The Zone&#46;</u>&ensp;
+                    </p>
+                    <p class="homeText">
+                        <span class="firstLetter">I</span> do not measure myself by what I have done<b>&#44;</b> but <em class="emSpecial">what I can and <em>will do</em><b>&#46;</b></em>&ensp;I thoroughly love to push myself beyond the boundaries that I have achieved to this point<b>&#46;</b>&ensp;Clint Eastwood as <b>Harry Callahan</b> said<b>&#58;</b>&ensp;<q class="italicsBold">A man<b class="apos"></b>s <span class="boldAllCaps">got</span> to know his limitations</q><b>&#46;</b>&ensp;Well<b>&#44;</b> I have not met my limitations<b>&#46;</b>&ensp;I am the type of person that when I see something that works<b>&#44;</b> I want to know <span class="italicsBold">&ldquo;who&rdquo;&#44;</span> <span class="italicsBold">&ldquo;what&rdquo;&#44;</span> <span class="italicsBold">&ldquo;when&rdquo;&#44;</span> <span class="italicsBold">&ldquo;where&rdquo;&#44;</span> <span class="italicsBold">&ldquo;how&rdquo;&#44;</span> <span class="italicsBold">&ldquo;why&rdquo;&#44;</span> 
+                        <span class="italicsBold">&ldquo;what for&rdquo;&#44;</span> and <span class="italicsBold">&ldquo;how come&rdquo;&#46;</span>&ensp;That should be the start of learning and creating<b>&#46;</b>
+                    </p>
+                    <blockquote>
+                        <h3>To quote Pablo Picasso&#58;</h3>
+                        <p><q>I am always doing that which I cannot do<b>&#44;</b> in order that I may learn how to do it&#46;</q></p>
+                        <p><q>Everything you can imagine is real&#46;</q></p>
+                    </blockquote>
+                    <p class="homeText">
+                        <span class="firstLetter">I</span> am very passionate about my artwork<b>&#46;</b>&ensp;This leads to maintaining and developing my skills and abilities<b>&#46;</b>&ensp;My skills as an artist are many<b>&#46;</b>&ensp;I have applied that same passion in acquiring and developing my skills for web and graphic design<b>&#46;</b>&ensp;
+                    </p>
+                    <p class="homeText">
+                        <span class="firstLetter">A</span>rt and design is my greatest passion<b>&#33;</b>&ensp;And has been the better part of my everyday life for several years<b>&#46;</b>&ensp;Over the past two years<b>&#44;</b> I have designed and worked on the creation of a web site to be used for educational purposes<b>&#46;</b>&ensp;A very large project<b>&#44;</b> that basically breaks down the <abbr class="htmlBold"></abbr> and <abbr class="html5"></abbr> specifications<b>&#44;</b> putting them into a student<b class="apos"></b>s perspective<b>&#46;</b>&ensp;I am the type of developer that does not like to take shortcuts<b>&#46;</b>&ensp;This means I am not quick to jump on <span class="italicsBold">open source/third party framework<b class="apos"></b>s&#44;</span> such as <span class="italics">AngularJS</span> or <span class="italics">Bootstrap</span><b>&#46;</b>&ensp;I would much rather design and custom write my own code that has the same functionality using 
+                        only <abbr class="html5"></abbr> and <abbr class="css3"></abbr><b>&#46;</b>&ensp;To me<b>&#44;</b> this allows for better flexibility<b>&#44;</b> faster page<span class="dashLine"></span>load times and structure maintenance<b>&#46;</b>&ensp;
+                    </p>
+                </div>
+                <nav class="miniSiteMap">
+                    <h3 class="home3"> What You Will find In This Web Site</h3>
+                    <div class="displayTable">
+                        <div class="displayTableRow">
+                            <p class="displayTableCellLink"><a href="myPortfolio/myArtwork.html">My Portfolio</a></p>
+                            <p class="displayTableCellLink"><a href="web&Graphics.php"><span class="bearzLogoBody"><span class="nowrap">Bear<span class="footerSmall">Z</span> &thinsp;Web <span class="bearzAmpersand"></span></span> <span class="nowrap">Gra<small class="footerSmall">F&thinsp;X</small> &thinsp;De<small class="footerSmall">Z</small>ign</span></span></a></p>
+                            <p class="displayTableCellLink"><a href="../TheHTMLZone/html_html5.html">The <abbr class="htmlBold"></abbr> Zone</a></p>
+                        </div>
+                        <div class="displayTableRow">
+                            <p class="displayTableCellLink"><a href="resume.php">My R&egrave;sum&egrave;</a></p>
+                            <p class="displayTableCellLink"><a href="splash.html"><span class="bearzLogoBody">Bear<span class="footerSmall">Z</span></span> &thinsp; <span class="nowrap">Coding Zone</span></a></p>
+                            <p class="displayTableCellLink"><a href="../TheCSSZone/css.html">The <abbr class="cssBold"></abbr> Zone</a></p>
+                        </div>
+                    	<p class="contactButtonContainer"><button class="contactButton" onClick="location.href='#popup1'" type="button">Contact Me&thinsp;&#33;</button></p>
+                    </div>
+                </nav>
+            </article>
+            <footer class="footer"><h5 class="bs"></h5>
+                <p>This Web Site has been created using the latest in <img class="footerImage" src="images/HTML5Logo.png" alt="Valid HTML5!"> Technology</p>
+                <p>Copyright <b>&copy;</b> 2014 &ndash; <?php echo date("Y"); ?></p>
+                <p><span class="bearzLogoFooter">Bear<span class="footerSmall">Z</span> &thinsp;Web <span class="bearzAmpersand"></span> Gra<small class="footerSmall">F&thinsp;X</small> &thinsp;De<small class="footerSmall">Z</small>ign</span></p>
+                <p class="bearzLogoFooter">BearZ DeZign ConCepts</p>
+                <p>All rights reserved<b>&#46;</b></p>
+                <p>This work is licensed under</p><p><a class="footerLink" rel="license external" href="http://creativecommons.org/licenses/by/4.0/" target="_blank"><span class="tipZoomIn">https://creativecommons.org/licenses/by/4.0/</span>Creative Commons Attribution 4.0 International License</a></p>
+                <p><script> 
+                    months = ['January', 'Febraury', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; 
+                    var d=new Date();
+                    var weekday=new Array(7); weekday[0]="Sunday"; weekday[1]="Monday"; weekday[2]="Tuesday"; weekday[3]="Wednesday"; weekday[4]="Thursday"; weekday[5]="Friday"; weekday[6]="Saturday";
+                    var d = new Date();
+                    (d.getFullYear());
+                    var theDate = new Date(document.lastModified); 
+                    theDate.setTime((theDate.getTime()+(60*60)) ) 
+                    with (theDate) { 
+                    document.write("<span class='italicsBold'>Last Modified<b>&#58;</b></span> <time class='modifiedDate'>"+weekday[getDay()]+'<b>&#44;&thinsp;</b>'+months[getMonth()]+'&thinsp;'+getDate()+'<b>&#44;&thinsp;</b>'+d.getFullYear()+'<b>&ensp;&#64;&ensp;</b>'+getHours()+':'+getMinutes()+"</time>&thinsp;&ensp;<span class='italicsBold'>EST</span>") } 
+                </script></p>
+            </footer>
+        </section>
+    </section>
+    <div id="popup1" class="overlay">
+        <div class="popup">
+            <h2 class="contactFormH2">email form</h2>
+            <a class="close" href="javascript:history.go(-1)">Close</a>
+            <div class="popupForm">
+                <form action="mailSent.php" method="post" name="form1" id="form1"  class="contactForm" onSubmit="MM_validateForm('from','','RisEmail','subject','','R','verif_box','','R','message','','R');return document.MM_returnValue">
+                    <span class="contactFormH4">Please Enter Your Name&#58;</span><hr class="hr-noLine-spacerPt5">
+                    <input name="name" type="text" id="name" class="contactFormName" value="<?php echo $_GET['name'];?>"/><hr class="hr-noLine-spacer1pt5">
+                    <span class="contactFormH4">Please Enter Your E-mail&#58;</span><hr class="hr-noLine-spacerPt5">
+                    <input name="from" type="text" id="from" class="contactFormEmail" value="<?php echo $_GET['from'];?>"/><hr class="hr-noLine-spacer1pt5">
+                    <span class="contactFormH4">Subject&#58;</span><hr class="hr-noLine-spacerPt5">
+                    <input name="subject" type="text" id="subject" class="contactFormSubject" value="<?php echo $_GET['subject'];?>"/><hr class="hr-noLine-spacer1pt5">
+                    <span class="contactFormH4">Please enter the code&#58;</span><hr class="hr-noLine-spacerPt5">
+                    <input name="verif_box" type="text" id="verif_box" class="contactFormVerify">
+                    <img src="verificationimage.php?<?php echo rand(0,9999);?>" alt="verification image, type it in the box" width="50" height="24" align="absbottom">
+                    <!--  if the variable "wrong_code" is sent from previous page then display the error field -->
+                    <?php if(isset($_GET['wrong_code'])){?>
+                    <div class="wrongCode">My Apologizes, Please Re-enter</div><hr class="hr-noLine"> 
+                    <?php ;}?><hr class="hr-noLine-spacer1pt5">
+                    <span class="contactFormH4">Please Enter Your Message&#58;</span><hr class="hr-noLine-spacerPt5">
+                    <textarea name="message" rows="5" cols="30" id="message" class="contactFormTextarea"><?php echo $_GET['message'];?></textarea><hr class="hr-noLine-spacer1pt5">
+                    <input name="Submit" type="submit" class="submit" value="Send Email"><hr class="hr-noLine-spacer1pt25">
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
